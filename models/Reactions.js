8104b0e6c067246bfdcae6513aeb6d3e3b307cbc
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-12-14 23:17:59 -0600

    Reactions model started

---
+++ models/Reactions.js
@@ -0,0 +1,22 @@
+const { Schema, model, types } = require("mongoose");
+
+const reactionsSchema = new Schema({
+  reactionId: {
+    type: Types.objectId,
+    default: new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal),
+  },
+});
