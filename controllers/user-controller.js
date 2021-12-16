60950c5d62063a6a636d7173208911d192e32ffc
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-12-15 20:14:37 -0600

    added delete user function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -46,6 +46,19 @@ const userController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  //Delete User
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this ID!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400), json(err));
+  },
 };
 
 module.exports = userController;
